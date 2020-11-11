701f9fdf072c2f48fad4546d883a9adba2ff0133
Artem <bondarchuk.artem20@gmail.com>
2020-11-12 01:30:31 +0200

    Update 9-collections.js
    added not working code

--- Syntax/9-collections.js
+++ Syntax/9-collections.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let phoneBook = [
+    {name: 'Marcus Aurelius', phone: '+380445554433'},
+    {name: 'Timur Shemsedinov', phone: '+380998887766'},
+    {name: 'James Bond', phone: '+380070071234'},
+    {name: 'Marcus Person', phone: '+380123123123'},
+    {name: 'Socrates', phone: '+380987654321'},
+];
+
+function findPhoneByName(name) {
+    for (name in phoneBook) {
+        return `Phone of ${name}: + `;
+    }
+}
+
+console.log(findPhoneByName('James Bond'));
