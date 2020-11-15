2c4caf32b037cd14bf899d20ae31909bcc1cee64
Artem <bondarchuk.artem20@gmail.com>
2020-11-15 13:08:16 +0200

    Update 2-generatekey.js

--- 3-Functions/2-generatekey.js
+++ 3-Functions/2-generatekey.js
@@ -1,5 +1,19 @@
 'use strict';
 
-function generateKey() {
-    
+const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
+
+function generateKey(length, characters) {
+    let keyArr = [];
+    for(let i = 0; i < length; i++) {
+        let xuy = random(0, characters.length);
+        keyArr.push(characters[xuy]);
+    }
+    return keyArr.join('');
+}
+
+const key = generateKey(15, characters);
+console.log(key);
