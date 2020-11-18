2b051497f3b1467ce68743b8a9e1aed1c13a0c6e
Artem <bondarchuk.artem20@gmail.com>
2020-11-18 20:19:13 +0200

    made it work

--- 3-Functions/3-IPv4.js
+++ 3-Functions/3-IPv4.js
@@ -1,2 +1,12 @@
 'use strict';
+ 
+function address (IPv4) {
+    return IPv4.split('.').map((el, index) => parseInt(el) << 8 * (3 - index)).reduce((a, b) => a + b);
+}
 
+console.log(address('127.0.0.1'));
+console.log(address('10.0.0.1'));
+console.log(address('192.168.1.10'));
+console.log(address('165.225.133.150'));
+console.log(address('0.0.0.0'));
+console.log(address('8.8.8.8'));
