ee617e7ca584680e4309b90df64a24fc91ee282f
Artem <bondarchuk.artem20@gmail.com>
2020-11-22 20:15:13 +0200

    added 'reduce' solution

--- 4-cycles-iteration/1-5-cycles.js
+++ 4-cycles-iteration/1-5-cycles.js
@@ -1,7 +1,15 @@
 'use strict';
 
+function sum(...args) {
+    return args.reduce((previous, current) => {
+        return previous + current;
+      }, 0);
+}
+
 const a = sum(1, 2, 3);
 const b = sum(0);
 const c = sum();
 const d = sum(1, -1, 1);
 const e = sum(10, -1, -1, -1);
+
+console.log(a, b, c, d, e);
