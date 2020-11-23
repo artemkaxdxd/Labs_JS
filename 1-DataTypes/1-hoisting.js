971c13418ddcb0bf147884b49cd7397becd9e01c
Artem <bondarchuk.artem20@gmail.com>
2020-11-23 18:36:17 +0200

    added more examples

--- 1-DataTypes/1-hoisting.js
+++ 1-DataTypes/1-hoisting.js
@@ -5,3 +5,15 @@ function myName(name) {
 }
 
 myName('Artem');
+
+function myAge(age) {
+    console.log(`I am ${age} years old.`);
+}
+
+myAge('17');
+
+function myCountry(country) {
+    console.log(`I am from ${country}.`);
+}
+
+myCountry('Ukraine');
