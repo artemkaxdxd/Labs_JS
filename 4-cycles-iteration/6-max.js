f8d4c3a8c700f660e53157313dc52b8b97b602e7
Artem <bondarchuk.artem20@gmail.com>
2020-11-26 15:58:54 +0200

    Finished example

--- 4-cycles-iteration/6-max.js
+++ 4-cycles-iteration/6-max.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const m = Math.max([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+function max (matrix) {
+    const max = (a, b) => (a > b ? a : b);
+    const res = matrix.map(row => row.reduce(max))
+    return console.log(Math.max(...res));
+}
 
-
-
-console.log(m);
+const m = max([
+    [1, 2, 3], 
+    [4, 5, 6], 
+    [7, 8, 9],
+]);
