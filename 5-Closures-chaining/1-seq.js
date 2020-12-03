ed7fa832751bb6b1dd2cd76cde36faa4a992c75d
Artem <bondarchuk.artem20@gmail.com>
2020-12-03 11:49:59 +0200

    Create 1-seq.js

---
+++ 5-Closures-chaining/1-seq.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const seq = a => b => typeof b === 'number' ? a(b) : seq(x => a(b(x)));
+
+const res1 = seq(x => x + 7)(x => x * 2)(5);
+const res2 = seq(x => x * 2)(x => x + 7)(5);
+const res3 = seq(x => x + 1)(x => x * 2)(x => x / 3)(x => x - 4)(7)
+console.log(res1);
+console.log(res2);
+console.log(res3);
