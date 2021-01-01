f38009de4c6ac7a6ad43cd76a2f7e6d438ea5cd4
Artem <bondarchuk.artem20@gmail.com>
2021-01-01 20:00:33 +0200

    Update 1-2-3-identifiers.js

--- 2-Syntax/1-2-3-identifiers.js
+++ 2-Syntax/1-2-3-identifiers.js
@@ -1,10 +1,16 @@
 'use strict';
 
-//let name = 'Artem';
-//const YEAR_BIRTH = 2003;
+let name = 'Artem';
+const YEAR_BIRTH = 2003;
 
 function hi(name) {
     console.log('Hello, ' + name);
 }
 
 hi('Artem');
+
+function greeting(greeting) {
+    console.log(greeting);
+}
+
+greeting('Happy New Year 2021!');
