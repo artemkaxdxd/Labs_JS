342e9da78cea7f66962caa781a666cb481f180c5
Artem <bondarchuk.artem20@gmail.com>
2021-01-02 20:35:44 +0200

    added solution

--- 6-Composition/2-compose.js
+++ 6-Composition/2-compose.js
@@ -1,6 +1,24 @@
 'use strict';
 
-const compose = (...fns) => {
-    // add solution
-    f.on('error', e => { console.error(); });
+function compose (...fns) {
+    const handlers = [];
+    function composed (x) {
+        if(fns.length === 0) return x;
+        const lastElem = fns.length - 1;
+        let res = x;
+        try {
+            for(let i = lastElem; i >= 0; i--){
+                res = fns[i](res);
+            }
+            return res;
+        } catch(error) {
+            for(const handler of handlers) {
+                handler(error);
+            }
+        }
+    };
+    composed.on = (name, handler) => {
+        if(name === 'error') handlers.push(handler);
+    };
+    return composed;
 };
