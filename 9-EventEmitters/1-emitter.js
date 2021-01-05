3ca2838e2b513de2d058211e96a2265a96081295
Artem <bondarchuk.artem20@gmail.com>
2021-01-05 14:54:45 +0200

    ee practice

---
+++ 9-EventEmitters/1-emitter.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const EventEmitter = require('events');
+
+const Logger = require('./2-logger');
+const logger = new Logger();
+
+//Register listener
+logger.on('messageLogged', (arg) => console.log('Listener called!', arg));
+
+logger.log('Happy New Year!');
