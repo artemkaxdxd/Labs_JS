bdad24965cfa29778196491e636e99fc45b36e9a
Artem <bondarchuk.artem20@gmail.com>
2021-01-09 12:12:44 +0200

    Create pallindrom.js

---
+++ ExamPreparation/pallindrom.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const isPalidrome = str => {
+    let strLen = str.length;
+    let result = '';
+    for(let i = 0; i < strLen; i++){
+        if(str[i] === str[strLen - 1 - i]) {
+            result = 'Yes';
+        } else {
+            result = 'No';
+        }
+        return result
+    }
+    return result;
+};
+
+let test1 = isPalidrome('abcdedcba');
+let test2 = isPalidrome('abcded');
+
+console.log(`Result of test1: ${test1}`);
+console.log(`Result of test2: ${test2}`);
