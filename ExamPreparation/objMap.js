6afed525899606ea6c55a3fe1509e45007f23861
Artem <bondarchuk.artem20@gmail.com>
2021-01-10 15:35:27 +0200

    optimized code

--- ExamPreparation/objMap.js
+++ ExamPreparation/objMap.js
@@ -1,15 +1,13 @@
 'use strict'
 
-let myObject = { 'a': 1, 'b': 2, 'c': 3 };
+const objectMap = (obj, callback) => Object.fromEntries(
+    Object.entries(obj).map(
+        ([key, val], i) => [key, callback(val, key, i)]
+    )
+);
 
-const objMap = (object, callback) => {
-    return Object.keys(object).reduce((result, key) => {
-        result[key] = callback(object[key]);
-        return result;
-    }, {})
-};
-
-let newObject = objMap(myObject, value => value * 2);
+const myObject = { a: 1, b: 2, c: 3 };
+const newObject = objectMap(myObject, v => v * v);
 
 console.log(myObject);
 console.log(newObject);
