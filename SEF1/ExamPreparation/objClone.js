4818781c975c2a4fa852f7b4453ed098b99170ba
Artem <bondarchuk.artem20@gmail.com>
2021-01-24 15:28:27 +0200

    cloning using function

--- SEF1/ExamPreparation/objClone.js
+++ SEF1/ExamPreparation/objClone.js
@@ -4,9 +4,22 @@ const obj = {
     food: 'chip',
     drink: 'bepsi',
     person: { me: 'Shrek' },
+    kek: () => 2,
+    test: { test: () => 3, },
+    symb: Symbol('4'),
 };
 
-let objClone = JSON.parse(JSON.stringify(obj));
+let objCloneJSON = JSON.parse(JSON.stringify(obj)); // cannot clone symbols and functions
 
-console.log(obj);
-console.log(objClone);
+function clone(obj) {
+    if (null === obj || "object" !== typeof obj) return obj;
+    let copy = obj.constructor();
+    for (let attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+    }
+    return copy;
+}
+
+console.log('Initial object:\n', obj, '\n');
+console.log('Cloning through JSON:\n', objCloneJSON, '\n');
+console.log('Cloning through function:\n', clone(obj), '\n');
